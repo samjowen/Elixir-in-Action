4e0e767e819baef4b58809fe181b3e4e8940372a
Samuel Owen <SamHirael@gmail.com>
2024-01-20 17:32:55 +0000

    Add entries function to retrieve data from todo list

--- test/todolist_test.exs
+++ test/todolist_test.exs
@@ -10,4 +10,13 @@ defmodule TodoListTest do
     my_assertion = %{~D[2024-01-22] => ["shower"]}
     assert my_assertion == TodoList.new() |> TodoList.add_entry(~D[2024-01-22], "shower")
   end
+
+  test "it can retrieve data" do
+    olistodo_list =
+      TodoList.new()
+      |> TodoList.add_entry(~D[2024-04-03], "guitar")
+
+    result_of_function = TodoList.entries(olistodo_list, ~D[2024-04-03])
+    assert result_of_function == ["guitar"]
+  end
 end
