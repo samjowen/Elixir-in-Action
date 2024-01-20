e4c2db48e0edb1260550d39946c64b17cb752152
Samuel Owen <SamHirael@gmail.com>
2024-01-20 14:33:13 +0000

    Add functionality to add new entries to the todo list

--- test/todolist_test.exs
+++ test/todolist_test.exs
@@ -5,4 +5,9 @@ defmodule TodoListTest do
     new_todo = TodoList.new()
     assert new_todo == %{}
   end
+
+  test "it can add a new entry" do
+    my_assertion = %{~D[2024-01-22] => ["shower"]}
+    assert my_assertion == TodoList.new() |> TodoList.add_entry(~D[2024-01-22], "shower")
+  end
 end
