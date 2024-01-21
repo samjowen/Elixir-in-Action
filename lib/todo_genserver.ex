4c9edf550ed553b770abbb74ff5001583cd7c1b3
Samuel Owen <SamHirael@gmail.com>
2024-01-21 01:31:05 +0000

    Add TodoServerGenServer module

---
+++ lib/todo_genserver.ex
@@ -0,0 +1,31 @@
+defmodule TodoServerGenServer do
+  use GenServer
+
+  @impl GenServer
+  def init(_) do
+    {:ok, TodoList.new()}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _from, state) do
+    {:reply, TodoList.entries(state, date), state}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, entry}, state) do
+    {:noreply, TodoList.add_entry(state, entry)}
+  end
+
+  # Public Interfaces
+  def start() do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def entries(date) do
+    GenServer.call(__MODULE__, {:entries, date})
+  end
+
+  def add_entry(entry) do
+    GenServer.call(__MODULE__, {:add_entry, entry})
+  end
+end
