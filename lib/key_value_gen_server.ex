54801d5cd64a5e24ae374c6a0783e7348267dc08
Samuel Owen <SamHirael@gmail.com>
2024-01-21 01:31:02 +0000

    Add cheat code functionality to GenServer

--- lib/key_value_gen_server.ex
+++ lib/key_value_gen_server.ex
@@ -13,6 +13,13 @@ defmodule KeyValueStoreGenServer do
   # Private
 
   @impl GenServer
+  def handle_info({:uduplrlrss}, unchanged_state) do
+    IO.puts("Cheat activated")
+    # State is unchanged, we just print something to the console
+    {:noreply, unchanged_state}
+  end
+
+  @impl GenServer
   def handle_cast({:put, key, value}, state) do
     {:noreply, Map.put(state, key, value)}
   end
@@ -24,11 +31,23 @@ defmodule KeyValueStoreGenServer do
 
   # Public Functions
 
+  def start() do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
   def get(pid, key) do
     GenServer.call(pid, {:get, key})
   end
 
+  @spec put(atom() | pid() | {atom(), any()} | {:via, atom(), any()}, any(), any()) :: :ok
   def put(pid, key, value) do
-    GenServer.call(pid, {:put, key, value})
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  # Does the Konami code on the GenServer
+  # This shows that we can send arbitrary messages to the GenServer and it will handle them
+  # by using the handle_info callback
+  def cheat(pid) do
+    send(pid, {:uduplrlrss})
   end
 end
