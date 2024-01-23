601ed8fab460e70ce125b6a820703e91dfc15b8a
Samuel Owen <SamHirael@gmail.com>
2024-01-23 21:40:23 +0000

    Add server creation in Todo.Cache module

--- lib/Todo/cache.ex
+++ lib/Todo/cache.ex
@@ -5,4 +5,21 @@ defmodule Todo.Cache do
     # Use an empty Map as the default state
     {:ok, %{}}
   end
+
+  # If the server exists, give it to the caller, if not, make it.
+  def handle_call({:server, name}, _from, state) do
+    case Map.fetch(state, name) do
+      {:ok, value} -> value
+      :error -> Todo.Server.start()
+    end
+  end
+
+  @spec start() :: :ignore | {:error, any()} | {:ok, pid()}
+  def start() do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def get_server(name) do
+    GenServer.call(__MODULE__, {:server, name})
+  end
 end
