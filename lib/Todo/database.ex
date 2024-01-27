f2e654b32d08374546f95f18e7961478d3408d31
Samuel Owen <SamHirael@gmail.com>
2024-01-27 12:03:28 +0000

    Refactor database module and worker
    This commit refactors the `Todo.Database` module and `Todo.DatabaseWorker` module. It introduces a new `choose_worker/1` function in the `Todo.Database` module to handle worker selection. The `store/2` and `get/1` functions in the `Todo.Database` module now use the `choose_worker/1` function to determine the worker to handle the request. The `Todo.DatabaseWorker` module now has separate functions for starting, storing, and retrieving data. This refactoring improves code organization and readability.

--- lib/Todo/database.ex
+++ lib/Todo/database.ex
@@ -7,44 +7,24 @@ defmodule Todo.Database do
     GenServer.start(__MODULE__, nil, name: __MODULE__)
   end
 
+  @spec store(any(), any()) :: any()
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    {:ok, worker_pid} = choose_worker(key)
+    Todo.DatabaseWorker.store(worker_pid, key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    {:ok, worker_pid} = choose_worker(key)
+    Todo.DatabaseWorker.get(worker_pid, key)
   end
 
   @impl GenServer
-  def handle_call({:get, key}, _from, state) do
-    data = File.read(file_name(key))
-
-    case data do
-      {:ok, contents} ->
-        :erlang.binary_to_term(contents)
-
-      _ ->
-        nil
-    end
-
-    {:reply, data, state}
-  end
-
-  @impl GenServer
-  def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, state}
-  end
-
-  defp file_name(key) do
-    Path.join(@db_folder, to_string(key))
+  def handle_call({:choose_worker, key}, _from, state) do
+    {:reply, choose_worker(key, state), state}
   end
 
   @spec choose_worker(any(), map()) :: {:ok, pid()} | {:error, String.t()}
-  def choose_worker(db_key, worker_map) do
+  defp choose_worker(db_key, worker_map) do
     worker_key = :erlang.phash2(db_key, map_size(worker_map))
 
     case Map.get(worker_map, worker_key) do
@@ -53,17 +33,22 @@ defmodule Todo.Database do
     end
   end
 
+  def choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
+  end
+
+  def start_workers() do
+    Enum.reduce(0..2, %{}, fn index, acc ->
+      {:ok, worker_pid} = Todo.DatabaseWorker.start(@db_folder)
+      Map.put(acc, index, worker_pid)
+    end)
+  end
+
   @impl GenServer
   @spec init(any()) :: {:ok, any()}
   def init(_) do
     File.mkdir_p!(@db_folder)
 
-    workers =
-      Enum.reduce(0..2, %{}, fn index, acc ->
-        {:ok, worker_pid} = Todo.DatabaseWorker.start(@db_folder)
-        Map.put(acc, index, worker_pid)
-      end)
-
-    {:ok, workers}
+    {:ok, start_workers()}
   end
 end
--- lib/Todo/database_worker.ex
+++ lib/Todo/database_worker.ex
@@ -1,13 +1,48 @@
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  @impl GenServer
-  def init(db_folder_path) do
-    {:ok, db_folder_path}
+  def start(db_folder) do
+    GenServer.start(__MODULE__, db_folder)
   end
 
-  @spec start(any()) :: :ignore | {:error, any()} | {:ok, pid()}
-  def start(db_folder_path) do
-    GenServer.start(__MODULE__, db_folder_path)
+  def store(worker_pid, key, data) do
+    GenServer.cast(worker_pid, {:store, key, data})
+  end
+
+  def get(worker_pid, key) do
+    GenServer.call(worker_pid, {:get, key})
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _from, db_folder) do
+    data = File.read(file_name(key, db_folder))
+
+    case data do
+      {:ok, contents} ->
+        :erlang.binary_to_term(contents)
+
+      _ ->
+        nil
+    end
+
+    {:reply, data, db_folder}
+  end
+
+  @impl GenServer
+  def handle_cast({:store, key, data}, db_folder) do
+    key
+    |> file_name(db_folder)
+    |> File.write!(:erlang.term_to_binary(data))
+
+    {:noreply, db_folder}
+  end
+
+  defp file_name(key, db_folder) do
+    Path.join(db_folder, to_string(key))
+  end
+
+  @impl GenServer
+  def init(db_folder) do
+    {:ok, db_folder}
   end
 end
